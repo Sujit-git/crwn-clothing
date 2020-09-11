314a55b1dace204cc2d368379213e0e9a440ac38
Sujit <sujitkhopade2001@gmail.com>
2020-09-11 00:03:00 -0700

    implementec

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component.jsx'
-import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in and sign-up.component.jsx'
-import { auth } from './firebase/firebase.utils';
+import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in-and-sign-up.component.jsx';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +20,26 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser : user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({ currentUser : user })
+      // createUserProfileDocument(user)
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      }
+
+      this.setState({
+        currentUser:userAuth
+      });
     })
   }
 
